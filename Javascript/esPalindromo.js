036d8218b8bb3fc4c805ae21beb9b559418ce09f
SrMule <eduardo.garciao@usm.cl>
2023-08-17 15:14:56 -0400

    nuevo comentario de ejercicio

---
+++ Javascript/esPalindromo.js
@@ -0,0 +1,4 @@
+/*
+Escribe una función que determine si una cadena es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda),
+ignorando espacios, mayúsculas y minúsculas.
+*/
