45a219ba48dd43a255ef61e876fbe3968f7acd52
SrMule <eduardo.garciao@usm.cl>
2023-08-17 15:59:37 -0400

    cambios en comentarios

--- Javascript/factorial.js
+++ Javascript/factorial.js
@@ -14,7 +14,7 @@ function factorial(num) {
     };
 };
 
-console.log(factorial(0));
-console.log(factorial(1));
-console.log(factorial(4));
-console.log(factorial(5));
+console.log(factorial(0)); //1
+console.log(factorial(1)); //1
+console.log(factorial(4)); //24
+console.log(factorial(5)); //120
