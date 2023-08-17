1b75d92e2c40b54d1e014b09438b4b34dd1407a9
SrMule <eduardo.garciao@usm.cl>
2023-08-17 15:28:50 -0400

    Esqueleto ejercicio palindromo

--- Javascript/esPalindromo.js
+++ Javascript/esPalindromo.js
@@ -2,3 +2,14 @@
 Escribe una función que determine si una cadena es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda),
 ignorando espacios, mayúsculas y minúsculas.
 */
+
+function esPalindromo(palabra1,palabra2) {
+
+};
+
+console.log(esPalindromo("Anilina, la niña liana")) //true
+console.log(esPalindromo("A mamá Roma le aviva el amor a papá y a papá Roma le aviva el amor a mamá")) //true
+console.log(esPalindromo("Anita atina")) //true
+console.log(esPalindromo("El cielo está despejado")) //false
+console.log(esPalindromo("La música suena hermosa en la mañana")) //false
+console.log(esPalindromo("La sal le da la luz azul al sol y a la luna la sal")) //true
