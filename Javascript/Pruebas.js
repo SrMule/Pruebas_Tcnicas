76cdf5d8ef2d1f0e155b168d30caa1ae6fceb57d
SrMule <eduardo.garciao@usm.cl>
2023-08-20 00:33:31 -0400

    Nuevas pruebas

--- Javascript/Pruebas.js
+++ Javascript/Pruebas.js
@@ -45,3 +45,71 @@ function sumTwoSmallestNumbers(numbers) {
 
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
 */
+
+/* 
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+
+
+function XO(str) {
+    str = str.toLowerCase();
+    arrX = [...str].filter((char) => char == "x");
+    arrY = [...str].filter((char) => char == "o");
+    if (arrX.length == arrY.length) {
+        return true
+    }
+    return false
+};
+
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+}
+
+console.log(XO("ooxx"));
+console.log(XO("xooxx"));
+console.log(XO("ooxXm"));
+console.log(XO("zpzpzpp)")); // when no 'x' and 'o' is present should return true
+console.log(XO("zzoo"));
+*/
+
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+
+function order(words){
+    var palabras = words.split(" ");
+    var sort = "";
+    for (let i = 1; i <= palabras.length; i++) {
+        for (let x = 0; x < palabras.length; x++) {
+            if (palabras[x].includes(i) && i != 1) {
+                sort += ` ${palabras[x]}`;
+            } else if (palabras[x].includes(i)){
+                sort += palabras[x];
+            };
+        };
+    };
+    return sort;
+};
+
+function order(words){
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+} 
+
+const order = words => words.split(' ').sort((a, b) => +/(\d+)/.exec(a)[0] - +/(\d+)/.exec(b)[0]).join(' ');
+
+
+console.log(order("is2 Thi1s T4est 3a"));
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+console.log(order(" "));
+*/
