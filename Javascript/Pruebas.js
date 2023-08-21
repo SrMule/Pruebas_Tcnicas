303db53c6928f1aa5dcb3519a9473bf6e39487fa
SrMule <eduardo.garciao@usm.cl>
2023-08-20 22:39:17 -0400

    Nueva prueba

--- Javascript/Pruebas.js
+++ Javascript/Pruebas.js
@@ -155,9 +155,90 @@ function alphabetPosition(text) {
     return text = newarr.join(" ");
 };
 
+let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split('').map(ch => ch.charCodeAt(0) - 64).join(' ');
+
 console.log(alphabetPosition("aBc d.")); // 1 2 3 4
 console.log(alphabetPosition("iom<:{b@")); // 9 15 13 2
 console.log(alphabetPosition("22b%6>?j")); // 2 10
 console.log(alphabetPosition("70*o#r:}")); // 15 18
 */
+
+/*
+Given an array of integers, find the one that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+    let times;
+    for (let i = 0; i < A.length; i++) {
+        times = A.filter(number => number == A[i])
+        if (times.length % 2 != 0) {
+            return A[i];
+        };
+    };
+};
+
+const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+function findOdd(arr) {
+  return arr.find((item, index) => arr.filter(el => el == item).length % 2)
+}
+
+
+console.log(findOdd([7]));
+console.log(findOdd([0]));
+console.log(findOdd([1,1,2]));
+console.log(findOdd([0,1,0,1,0]));
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));
+console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]));
+
+*/
+
+/*
+Tribonacci
+you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array
+
+
+function tribonacci(signature,n){
+    if (n < 3) {
+        return signature.splice(0,n);
+    } else {
+        for (let i = signature.length-1; i < n-1; i++) {
+            signature.push(signature[i-2] + signature[i-1] + signature[i]);
+        };
+        return signature;
+    };
+};
+
+console.log(tribonacci([1,1,1], 10));
+console.log(tribonacci([0,0,1], 10));
+console.log(tribonacci([1,1,1],  1));
+console.log(tribonacci([300,200,100], 0));
+
+*/
+
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+The string has a length greater or equal to one and contains only letters from ato z.
+
+function printerError(s) {
+    let error = s.split("").filter((char => /^[n-z]+$/.test(char)))
+    return `${error.length}/${s.length}`;
+};
+
+function printerError(s) {
+    return `${s.split("").filter((char => /^[n-z]+$/.test(char))).length}/${s.length}`;
+};
+
+const printerError = s => `${s.split("").filter((char => /^[n-z]+$/.test(char))).length}/${s.length}`;
+const printerError = s => `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
+
+console.log(printerError("aaabbbbhaijjjm")); // 0/14
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm")); // 8/22
+
+*/
+
 
